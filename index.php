a7c85ecf64440b33fb522a2f2cf4a6615d1415b4
Dgirunies <erineu16@gmail.com>
2022-02-25 16:06:28 +0100

    Adding Header and Page Cover

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./src/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./src/css/geral.css" />
+  <link rel="stylesheet" href="./src/css/header.css" />
+  <title>PCCP</title>
+</head>
+
+<body>
+  <header class="toolbar">
+    <nav class="toolbarNav">
+      <div class="toolbarNavLeft">
+        <a href="index.php"><span class="red">P</span><span class="yellow">C</span><span class="yellow">C</span><span>P</span></span></a>
+      </div>
+      <div class="toolbarNavRight">
+        <ul>
+          <li>
+            <a href="#home">Início</a>
+          </li>
+          <li>
+            <a href="#about">Sobre</a>
+          </li>
+          <li>
+            <a href="#announcements">Anúncios</a>
+          </li>
+          <li>
+            <a href="#contacts">Contactos</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <div class="indexCover">
+      <div class="indexCoverBackdrop"></div>
+      <div class=" indexCoverDescription">
+        <h1>Plataforma para Candidaturas</h1>
+        <h2>O Seu Sucesso Começa Aqui!</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-"></div>
+    </div>
+  </main>
+</body>
+
+</html>
